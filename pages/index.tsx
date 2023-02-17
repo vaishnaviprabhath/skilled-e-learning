f139e302b3b8fe5ece8741eab0df2ba270abfaac
vaishnaviprabhath <chvkarmy@gmail.com>
2023-02-17 16:55:42 +0530

    added button

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,9 +9,22 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Sample App</title>
+        <title>Frontend Mentor | Skilled e-learning landing page</title>
       </Head>
-      <p className="text-lg font-plusjakarta">Sample text in lg</p>
+      <main>
+        <div className="flex flex-col items-center pt-4">
+          <p className="text-lg font-plusjakarta font-extrabold text-[28px] text-vdb">
+            skilled
+          </p>
+        </div>
+        <div className="flex flex-col items-center justify-center">
+          <button className="flex h-12 w-[140px] flex-row items-center justify-center rounded-full bg-vdb  ">
+            <span className="font-plusjakarta font-bold text-white ">
+              Get started{" "}
+            </span>{" "}
+          </button>{" "}
+        </div>
+      </main>
     </>
   );
 }
