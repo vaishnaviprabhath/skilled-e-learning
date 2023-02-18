47ae09ba1181d9670d2adbbae4e4e08ffb8beccd
vaishnaviprabhath <chvkarmy@gmail.com>
2023-02-18 10:40:14 +0530

    added button and text on same line

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,17 +12,17 @@ export default function Home() {
         <title>Frontend Mentor | Skilled e-learning landing page</title>
       </Head>
       <main>
-        <div className="flex flex-col items-center pt-4">
-          <p className="text-lg font-plusjakarta font-extrabold text-[28px] text-vdb">
-            skilled
-          </p>
-        </div>
-        <div className="flex flex-col items-center justify-center">
-          <button className="flex h-12 w-[140px] flex-row items-center justify-center rounded-full bg-vdb  ">
-            <span className="font-plusjakarta font-bold text-white ">
-              Get started{" "}
-            </span>{" "}
-          </button>{" "}
+        <div className="pt-6 ">
+          <div className="flex items-center justify-center  ">
+            <p className=" font-plusjakarta font-extrabold text-[28px] text-vdb pr-16">
+              skilled
+            </p>
+            <button className="flex-row  h-12 w-[140px] rounded-full bg-vdb  ">
+              <span className="font-plusjakarta font-bold text-white ">
+                Get started{" "}
+              </span>{" "}
+            </button>{" "}
+          </div>
         </div>
       </main>
     </>
