bd1a1fa076f3e9032cd1cfff4eebebea6bfe9c46
vaishnaviprabhath <chvkarmy@gmail.com>
2023-02-18 11:48:41 +0530

    "added padding "

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,11 +12,16 @@ export default function Home() {
         <title>Frontend Mentor | Skilled e-learning landing page</title>
       </Head>
       <main>
-        <div className="pt-6 ">
-          <div className="flex items-center justify-center  ">
-            <p className=" font-plusjakarta font-extrabold text-[28px] text-vdb pr-16">
-              skilled
-            </p>
+        <div className="pt-4 px-4">
+          <div className="flex items-center justify-between  ">
+            <div className="">
+              <Image
+                width={119}
+                height={28}
+                src="/assets/logo-dark.svg"
+                alt="Logo"
+              ></Image>
+            </div>
             <button className="flex-row  h-12 w-[140px] rounded-full bg-vdb  ">
               <span className="font-plusjakarta font-bold text-white ">
                 Get started{" "}
